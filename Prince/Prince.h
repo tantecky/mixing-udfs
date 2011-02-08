4cfe5ccf84eb8e515dfa4545e9bdc706e1ebd01e
6killers <6killers@localhost>
2011-02-08 12:01:24 +0100

    Prince model added

---
+++ Prince/Prince.h
@@ -0,0 +1,26 @@
+#ifndef _PRINCE_H_
+#define _PRINCE_H_
+
+#include "udf.h"
+#include "udf.h"
+#include "sg_pb.h"
+#include "sg_mphase.h"
+#include <gsl/gsl_integration.h>
+#include <gsl/gsl_errno.h>
+
+/* GSL je cela double */
+#if RP_DOUBLE == 0
+#error "UDF vyzaduje double precision mode"
+#endif
+
+typedef struct ParametryFce
+{
+    real eps;
+    real alpha;
+    real d_1;
+
+} ParametryFce;
+
+real IntegrovanaFce(real, void *);
+
+#endif
