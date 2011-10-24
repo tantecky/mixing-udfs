db8b80c0e8a4dc75eada59ca1a808540763c7215
6killers <6killers@localhost>
2011-10-24 15:00:22 +0200

    PyrolysisGA

---
+++ PyrolysisGA/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+double suma(double a, double b)
+{
+    return a + b;
+}
+
+int main()
+{
+   // cout << "Hello world!" << endl;
+
+    double neco = suma(5, 6);
+
+    //neco = neco + 2;
+    neco += 2;
+
+    bool logic = false;
+
+    if(logic == true)
+    {
+        cout << "ahoj";
+    }
+    else
+    {
+        cout << "cau";
+    }
+
+
+
+    return 0;
+}
