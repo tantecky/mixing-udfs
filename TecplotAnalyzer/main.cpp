d0523ce84d1007c5f14a5792f35f6446771b0361
6killers <6killers@localhost>
2011-11-09 22:48:37 +0100

    Analyzer edited

--- TecplotAnalyzer/main.cpp
+++ TecplotAnalyzer/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include "DataFile.hpp"
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+    try
+    {
+        DataFile dataFile;
+
+        dataFile.LoadDataFile("Tomtom2.dat");
+
+
+        return 0;
+    }
+    catch(std::exception& e)
+    {
+
+        std::cerr << "Exception has arised:" << std::endl;
+        std::cerr << e.what() << std::endl;
+
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
