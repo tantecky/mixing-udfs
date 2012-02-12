f24724f607aeb3dfbc3acfa5af6dc1b1776c3a2f
6killers <6killers@localhost>
2012-02-12 12:41:17 +0100

    development

--- DragCoefModels/Models.c
+++ DragCoefModels/Models.c
@@ -220,7 +220,7 @@ real QualityOfSuspension()
         {
             frac = C_VOF(c, THREAD_SUB_THREAD(t, SOLID_PHASE_ID));
 
-            parcSum += pow(frac/C_VOLUME(c,t) - avgVolFrac/totalVolume, 2.0);
+            parcSum += pow(frac/C_VOLUME(c,t) - sumVolFrac/totalVolume, 2.0);
 
         }
         end_c_loop(c,t)
