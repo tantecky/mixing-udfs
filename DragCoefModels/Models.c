b13025c3f03664c820497c096be22811e60d3d20
6killers <6killers@localhost>
2012-04-24 21:19:52 +0200

    Added support for MS compiler + source cleaned up

--- DragCoefModels/Models.c
+++ DragCoefModels/Models.c
@@ -6,15 +6,22 @@ Several drag coefficient models for solid-liquid interphase drag force
     - Khopkar
 
 Author: Tomáš Antecký (21. 10. 2011)
-Develop for GCC according to gnu90 standard
+Developed for GCC and MS compiler
+Tested on Fluent 12.1.4
 */
 
 #include <math.h>
-#include <stdint.h>
 #include <udf.h>
 #include <sg_mphase.h>
 #include <sg.h>
 
+#ifdef _MSC_VER /*for MS compiler*/
+#include <float.h>
+#define isfinite(x) _finite(x)
+#define isnan(x) _isnan(x)
+#endif
+
+/*======SETTINGS STARTS HERE======*/
 /*physical properties*/
 #define DIAMETER (1.02e-3) /*diameter of the solid particle*/
 #define RHO_L (999.5) /*density of the liquid phase*/
@@ -23,23 +30,25 @@ Develop for GCC according to gnu90 standard
 
 #define SOLID_PHASE_ID (1) /*id of the solid phase*/
 
-/*checking if exchange coefficient is a finite number*/
+/*checking if computed exchange coefficient is a finite number*/
 #define DEBUG_COEF
 
 /*if is defined the drag coeffcient is stored in user defined memory*/
+/*don't forget to allocate memory for it under:
+  Define -> User-Defined -> Memory*/
 #define USE_UDM
 
 #ifdef USE_UDM
 #define CD_UDM_NUM (0) /*the id of the drag coefficient in UDM*/
-#define QA_UDM_NUM (1) /*the id of the quality of suspension in UDM*/
 #endif
+/*======SETTINGS ENDS HERE======*/
 
 #ifdef DEBUG_COEF
 #define CHECK_COEF(result) \
-if(!isfinite(result) || result < 0.0) { \
-   fprintf(stderr, "\nDetected: %e in %s:%i\n", result, __FUNCTION__, __LINE__); \
-   abort(); } \
-   (void)0
+    if(!isfinite(result) || result < 0.0) { \
+       fprintf(stderr, "\nDetected: %e in %s:%i\n", result, __FUNCTION__, __LINE__); \
+       abort(); } \
+       (void)0
 #else
 #define CHECK_COEF(result) (void)0
 #endif
@@ -55,13 +64,16 @@ DEFINE_EXECUTE_ON_LOADING(on_load, libname)
 
 DEFINE_EXCHANGE_PROPERTY(SchillerNauman_CD, cell, mix_thread, s_col, f_col)
 {
+    real vol_s;
+    real k_s_l;
+
     /*cd0 is Schiller-Nauman's drag coefficinet*/
 #include "SchillerNauman.h"
 
     /*volumetric fraction of the solid phase*/
-    real vol_s = C_VOF(cell, thread_s);
+    vol_s = C_VOF(cell, thread_s);
 
-    real k_s_l = (3./4.)*vol_s/DIAMETER*cd0*RHO_L*slip;
+    k_s_l = (3./4.)*vol_s/DIAMETER*cd0*RHO_L*slip;
 
     if(isnan(k_s_l))
         return 0.0;
@@ -78,25 +90,32 @@ DEFINE_EXCHANGE_PROPERTY(SchillerNauman_CD, cell, mix_thread, s_col, f_col)
 
 DEFINE_EXCHANGE_PROPERTY(Pinelli_CD, cell, mix_thread, s_col, f_col)
 {
+    real eps;
+    real kolscale;
+    real cd;
+    real vol_s;
+    real k_s_l;
+    real pinelli;
+
     /*cd0 is Schiller-Nauman's drag coefficinet*/
 #include "SchillerNauman.h"
 
     /*---Pinelli Correction---*/
 
     /*energy dissipation rate*/
-    real eps = C_D(cell,thread_l);
+    eps = C_D(cell,thread_l);
 
     /*Kolmogoroff microscale*/
-    real kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
+    kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
 
-    real pinelli = 0.4*tanh(16.*kolscale/DIAMETER - 1.) + 0.6;
+    pinelli = 0.4*tanh(16.*kolscale/DIAMETER - 1.) + 0.6;
 
-    real cd = cd0/pow(pinelli, 2.0);
+    cd = cd0/pow(pinelli, 2.0);
 
     /*volumetric fraction of the solid phase*/
-    real vol_s = C_VOF(cell, thread_s);
+    vol_s = C_VOF(cell, thread_s);
 
-    real k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
+    k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
 
     if(isnan(k_s_l))
         return 0.0;
@@ -113,23 +132,29 @@ DEFINE_EXCHANGE_PROPERTY(Pinelli_CD, cell, mix_thread, s_col, f_col)
 
 DEFINE_EXCHANGE_PROPERTY(Brucato_CD, cell, mix_thread, s_col, f_col)
 {
+    real eps;
+    real kolscale;
+    real cd;
+    real vol_s;
+    real k_s_l;
+
     /*cd0 is Schiller-Nauman's drag coefficinet*/
 #include "SchillerNauman.h"
 
     /*---Brucato Correction---*/
 
     /*energy dissipation rate*/
-    real eps = C_D(cell,thread_l);
+    eps = C_D(cell,thread_l);
 
     /*Kolmogoroff microscale*/
-    real kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
+    kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
 
-    real cd = cd0*(1 + 8.76e-4*pow(DIAMETER/kolscale, 3.0));
+    cd = cd0*(1 + 8.76e-4*pow(DIAMETER/kolscale, 3.0));
 
     /*volumetric fraction of the solid phase*/
-    real vol_s = C_VOF(cell, thread_s);
+    vol_s = C_VOF(cell, thread_s);
 
-    real k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
+    k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
 
     if(isnan(k_s_l))
         return 0.0;
@@ -146,23 +171,29 @@ DEFINE_EXCHANGE_PROPERTY(Brucato_CD, cell, mix_thread, s_col, f_col)
 
 DEFINE_EXCHANGE_PROPERTY(Khopkar_CD, cell, mix_thread, s_col, f_col)
 {
+    real eps;
+    real kolscale;
+    real cd;
+    real vol_s;
+    real k_s_l;
+
     /*cd0 is Schiller-Nauman's drag coefficinet*/
 #include "SchillerNauman.h"
 
     /*---Khopkar Correction---*/
 
     /*energy dissipation rate*/
-    real eps = C_D(cell,thread_l);
+    eps = C_D(cell,thread_l);
 
     /*Kolmogoroff microscale*/
-    real kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
+    kolscale = pow((NU_L*NU_L*NU_L)/eps, 0.25);
 
-    real cd = cd0*(1 + 8.76e-5*pow(DIAMETER/kolscale, 3.0));
+    cd = cd0*(1 + 8.76e-5*pow(DIAMETER/kolscale, 3.0));
 
     /*volumetric fraction of the solid phase*/
-    real vol_s = C_VOF(cell, thread_s);
+    vol_s = C_VOF(cell, thread_s);
 
-    real k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
+    k_s_l = (3./4.)*vol_s/DIAMETER*cd*RHO_L*slip;
 
     if(isnan(k_s_l))
         return 0.0;
@@ -181,12 +212,15 @@ real MeanVolFrac(void)
     Domain* d;
     Thread *t;
     cell_t c;
-    d = Get_Domain(1);
-
-    uint_least32_t numOfCells = 0;
-    real totalVolume = 0.0;
-    real sumVolFrac = 0.0;
+    unsigned int numOfCells;
+    real totalVolume;
+    real sumVolFrac;
     real cellVol;
+
+    numOfCells = 0;
+    totalVolume = 0.0;
+    sumVolFrac = 0.0;
+    d = Get_Domain(1);
 
     thread_loop_c(t,d)
     {
@@ -210,13 +244,15 @@ real QualityOfSuspension()
     Domain* d;
     Thread *t;
     cell_t c;
-    d = Get_Domain(1);
-
-    uint_least32_t numOfCells = 0;
-    real avgVolFrac = MeanVolFrac();
-
     real frac;
-    real parcSum = 0.0;
+    unsigned int numOfCells;
+    real avgVolFrac;
+    real parcSum;
+
+    numOfCells = 0;
+    avgVolFrac = MeanVolFrac();
+    parcSum = 0.0;
+    d = Get_Domain(1);
 
     thread_loop_c(t,d)
     {
@@ -236,33 +272,13 @@ real QualityOfSuspension()
 
 }
 
-#ifdef USE_UDM
-DEFINE_EXECUTE_AT_END(Quality_of_suspension)
-{
-    Domain* d;
-    Thread* t;
-    cell_t c;
-    d = Get_Domain(1);
-
-    real qa = QualityOfSuspension();
-
-    /*Message0("\nQualityOfSuspension: %f\n", qa);*/
-
-    thread_loop_c(t,d)
-    {
-        begin_c_loop(c,t)
-        {
-            C_UDMI(c, t, QA_UDM_NUM) = qa;
-        }
-        end_c_loop(c,t)
-
-    }
-}
-#endif
-
+/*compute and display on demand quality of suspension*/
+/*Define -> User-Defined -> Execute on Demand*/
 DEFINE_ON_DEMAND(QA_of_suspension)
 {
-    real qa = QualityOfSuspension();
+    real qa;
+
+    qa = QualityOfSuspension();
 
     Message0("\nQualityOfSuspension: %f\n", qa);
 }
--- DragCoefModels/SchillerNauman.h
+++ DragCoefModels/SchillerNauman.h
@@ -1,21 +1,30 @@
-/*liquid phase - primary*/
-Thread* thread_l = THREAD_SUB_THREAD(mix_thread, s_col);
-/*solid phase - secondary*/
-Thread* thread_s = THREAD_SUB_THREAD(mix_thread, f_col);
+Thread* thread_l;
+Thread* thread_s;
 
-real slip_x = C_U(cell, thread_l) - C_U(cell, thread_s);
-real slip_y = C_V(cell, thread_l) - C_V(cell, thread_s);
-real slip_z = C_W(cell, thread_l) - C_W(cell, thread_s);
+real slip_x;
+real slip_y;
+real slip_z;
+real slip;
 
-/*Euclidean norm of slip velocity*/
-real slip = sqrt(slip_x*slip_x + slip_y*slip_y + slip_z*slip_z);
-
-/*relative Reynolds number for the primary phase*/
-real reyp = RHO_L*slip*DIAMETER/MU_L;
-
-/*drag coefficient*/
+real reyp;
 real cd0;
 
+/*liquid phase - primary*/
+thread_l = THREAD_SUB_THREAD(mix_thread, s_col);
+/*solid phase - secondary*/
+thread_s = THREAD_SUB_THREAD(mix_thread, f_col);
+
+slip_x = C_U(cell, thread_l) - C_U(cell, thread_s);
+slip_y = C_V(cell, thread_l) - C_V(cell, thread_s);
+slip_z = C_W(cell, thread_l) - C_W(cell, thread_s);
+
+/*Euclidean norm of slip velocity*/
+slip = sqrt(slip_x*slip_x + slip_y*slip_y + slip_z*slip_z);
+
+/*relative Reynolds number for the primary phase*/
+reyp = RHO_L*slip*DIAMETER/MU_L;
+
+/*drag coefficient*/
 /*Schiller-Nauman*/
 if(reyp > 1000.)
     cd0 = 0.44;
